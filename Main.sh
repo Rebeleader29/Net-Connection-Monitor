202a73130826346258963ccd72b3c14a554f9c2b
Rebeleader29 <aarjamstu@gmail.com>
2025-04-27 23:56:25 -0400

    Set up pinging on host

--- Main.sh
+++ Main.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-Error = 0
+Error=0
 
 #Sets for break on error, otherwise continuous loop
-for Error in 0
+while [ $Error == 0 ]
 do
-ping 1.1.1.1 
+Ping=$(ping -c 1 1.1.1.1)
+clear
+echo $Ping
+sleep 10
 done
